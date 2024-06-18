238ba2934e6d5f3ac49361c97c017370db32e541
TiThuthur <thurthur.petit.py@gmail.com>
2024-06-18 15:27:56 +0200

    création de la page gliph

--- app/gliph/page.tsx
+++ app/gliph/page.tsx
@@ -2,9 +2,9 @@
 
 import Image from "next/image";
 import { hiraganaGliph } from "../model/hiraganaGliph";
+import { katakanaGliph } from "../model/katakanaGliph";
 import { useState } from "react";
 import { Button, Menu, Card } from "antd";
-import { katakanaGliph } from "../model/katakanaGliph";
 
 const items = [
   { label: "Hiragana", key: "hiragana" },
@@ -17,7 +17,7 @@ export default function Home() {
   const [katakanaGliphObject, setKatakanaGliphObject] = useState<{[syllabes:string]:string}>(katakanaGliph);
   const [objectGliph, setObjectGliph] = useState<{[syllabes:string]:string}>(hiraganaGliphObject);
   const [index, setIndex] = useState<number>(0);
-  
+  const [key,setKey] = useState<string>("a")
 
   const attributionObject = (current: string) => {
     if (current === "hiragana") {
@@ -29,29 +29,25 @@ export default function Home() {
     }
   };
 
-  const newIndex = () => {
+  const newKey = () => {
     setIndex(Math.floor(Math.random() * Object.keys(objectGliph).length));
+    setKey(Object.keys(objectGliph)[index])
   }
   
-  const sliceList = ()=>{
-    setList(list.slice(0, index).concat(list.slice(index+1)));
-    console.log(list);
+  const deleteItem = (key: string )=>{
+    const {[key]:_,...newItem} = objectGliph
+    setObjectGliph(newItem);
   }
 
-  const onClickWithSlice = (e: any) => {
-    newIndex()
-    sliceList()
+  const onClickWithDelete = (e: any) => {
+    let key = Object.keys(objectGliph)[index]
+    deleteItem(key)
+    newKey()
   };
   const onClickNoSlice = (e: any) => {
     setCurrent(e.key);
-    attributionList(e.key);
-    newIndex()
-  };
-
-  const onRegen = () => {
-    setHiraganaList(hiragana);
-    setKatakanaList(katakana);
-    attributionList(current);
+    attributionObject(e.key);
+    newKey()
   };
 
   return (
@@ -69,24 +65,22 @@ export default function Home() {
         <Card
           title={current}
           className={"size-60 rounded-lg border border-slate-400 bg-slate-200 p-2 text-center text-7xl " + (index % 2 ===0 ? "text-red-700":"text-emerald-700")}>
-          {list[index]}
+          {objectGliph[key]}
         </Card>
         <div className="flex items-center justify-center gap-4">
           <Button
             size="large"
-            onClick={onClickWithSlice}
+            onClick={onClickWithDelete}
           >
             Next
           </Button>
-          <Button size="large" onClick={onRegen}>
-            Regen
-          </Button>
+          
         </div>
       </main>
       <Card title={current} className="text-wrap">
         <div className=" grid grid-cols-10 gap-2">
-          {list.map((item, index) => (
-            <p key={index} className={index % 2===0 ? "text-red-700":"text-emerald-700"}>{item},</p>
+          {Object.entries(objectGliph).map(([key,item], index) => (
+            <p key={key} className={index % 2===0 ? "text-red-700":"text-emerald-700"}>{item},</p>
           ))}
         </div>
       </Card>
